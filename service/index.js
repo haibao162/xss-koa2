2d197b1692e042661ec6e8eeb1ffad2bd3646fd9
Jiaxing <jiaxing.yang@guanaitong.com>
2021-09-29 18:05:07 +0800

    fix ui

--- service/index.js
+++ service/index.js
@@ -4,17 +4,6 @@ const service = axios.create({
     baseURL: "https://open.feishu.cn", // api的base_url  process.env.BASE_API,,注意局域网访问时，不能使用localhost
     timeout: 20 * 1000 // 请求超时时间
 })
-// "http://127.0.0.1:3000"
-
-function sendMessage() {
-    return service({
-        url: '/open-apis/bot/hook/16f6b80d48f04cb1bf43a0cff4e17d55',
-        method: 'post',
-        data: {
-            a: 1
-        },
-    });
-}
 
 function getString() {
     return service({
